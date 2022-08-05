84aae2d126387deb6b60db4dc142fb76e3c7dd49
Yubing <yubinnng@outlook.com>
2022-08-05 11:09:35 +0100

    Rename workflow engine dir to `walle-engine`

--- template/walle-engine/event/event.go
+++ template/walle-engine/event/event.go
@@ -8,14 +8,16 @@ import (
 
 var client *nats.Conn
 
+const NATS_URL = "nats://nats.walle:4222"
+
 func ConnectNATS() {
 	// Connect to a server
-	nc, err := nats.Connect(nats.DefaultURL)
+	nc, err := nats.Connect(NATS_URL)
 	if err != nil {
-		log.Fatal("Failed to connect to NATS")
+		log.Fatal("Failed to connect to NATS, URL=" + NATS_URL)
 	}
 	client = nc
-	log.Println("Connected to NATS")
+	log.Println("Connected to NATS, URL=" + NATS_URL)
 }
 
 func Publish(topic string, data []byte) {
